6bd05c4721f9936552dd0b0b37a4253b1b99ecef
NadiaMoosavifar <moosavifar.nadia@gmail.com>
2020-09-05 19:43:15 +0200

    commit all flie Assignment-Turig to Gates

---
+++ js.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#firstImg").on('click',function(){
+        $("#firstImg").attr('src', "./image/1b.jpg");
+      });
+      $("#secondImg").on('click',function(){
+        $("#secondImg").attr('src', "./image/2b.jpg");
+      });
+    $("#thirdImg").on('click',function(){
+        $("#thirdImg").attr('src', "./image/3b.jpg");
+      });
+    })
